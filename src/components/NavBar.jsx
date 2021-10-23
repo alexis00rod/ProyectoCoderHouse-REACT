b63e1303d79f4cb93db849a640483dfbf3a84594
Alexis Ruiz Diaz <alexis00rodrigo@gmail.com>
2021-10-23 20:27:54 -0300

    Corregi los warnings de la consola

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,32 +8,31 @@ export const NavBar = () => {
                 <li className="NavBar__menu-item">
                     <span>PS3</span>
                     <ul className="NavBar__dropdown-menu">
-                        <li><a href="#">All</a></li>
-                        <li><a href="#">Best Seller</a></li>
-                        <li><a href="#">Offer</a></li>
+                        <li><a href="#/">All</a></li>
+                        <li><a href="#/">Best Seller</a></li>
+                        <li><a href="#/">Offer</a></li>
                     </ul>
                 </li>
                 <li className="NavBar__menu-item">
                     <span>PS4</span>
                     <ul className="NavBar__dropdown-menu">
-                        <li><a href="#">All</a></li>
-                        <li><a href="#">Best Seller</a></li>
-                        <li><a href="#">Offer</a></li>
+                        <li><a href="#/">All</a></li>
+                        <li><a href="#/">Best Seller</a></li>
+                        <li><a href="#/">Offer</a></li>
                     </ul>
                 </li>
                 <li className="NavBar__menu-item">
                     <span>PS5</span>
                     <ul className="NavBar__dropdown-menu">
-                        <li><a href="#">All</a></li>
-                        <li><a href="#">Best Seller</a></li>
-                        <li><a href="#">Offer</a></li>
+                        <li><a href="#/">All</a></li>
+                        <li><a href="#/">Best Seller</a></li>
+                        <li><a href="#/">Offer</a></li>
                     </ul>
                 </li>
                 <li className="NavBar__menu-item">
                     <CartWidget />
                 </li>
-            </ul>
-            
+            </ul> 
         </nav>
     )
 }
