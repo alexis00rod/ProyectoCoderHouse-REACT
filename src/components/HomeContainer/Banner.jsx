ef3338b28ecece51f9eaddffc92cc756355a2f77
Alexis Ruiz Diaz <alexis00rodrigo@gmail.com>
2021-12-06 15:17:59 -0300

    Add loader

--- src/components/HomeContainer/Banner.jsx
+++ src/components/HomeContainer/Banner.jsx
@@ -1,26 +1,34 @@
 import { Link } from "react-router-dom"
-import { getFirestore } from "../../services/getFirestone"
 import { useState, useEffect } from "react"
+import { dbCollectionItems } from "../../services/getFirestone"
+import { Loader } from "../Loader/Loader"
 
 export const Banner = () => {
+    const [loading, setLoading] = useState(true)
     const [banner, setBanner] = useState({})
 
     useEffect(() => {
-        const db = getFirestore()
-        const dbQuery = db.collection("items").get()
+        const dbQuery = dbCollectionItems.get()
         dbQuery
         .then(resp => setBanner(resp.docs[0].data()))
+        .finally(() => setLoading(false))
 
     },[])
 
     return (
-        <div className="banner">
-            <img src={banner.imgBanner} alt="" />
-            <div className="banner-info">
-                <h2 className="textColor">Buy games easily and safely.</h2>
-                {/* <button className="btn__primary"><Link to="/shop" >See catalog</Link></button> */}
-                <button className="btn-primary"><Link to="/shop" >See catalog</Link></button>
-            </div>
-        </div>
+        <>
+        {
+            loading ? 
+            <Loader /> 
+            :
+            <div className="banner">
+                <img src={banner.imgBanner} alt="" />
+                <div className="banner-info">
+                    <h2 className="textColor">Buy games easily and safely.</h2>
+                    <button className="btn-primary"><Link to="/shop" >See catalog</Link></button>
+                </div>
+            </div> 
+        }
+        </>
     )
 }
--- src/components/HomeContainer/Slide.jsx
+++ src/components/HomeContainer/Slide.jsx
@@ -1,20 +1,22 @@
-import { getFirestore } from "../../services/getFirestone"
+import { dbCollectionItems } from "../../services/getFirestone"
 import { useState, useEffect, useRef } from "react"
 import { Link } from "react-router-dom"
 import { SlideItem } from "./SlideItem"
+import { Loader } from "../Loader/Loader"
 
 export const Slide = ({category}) => {
+    const [loading, setLoading] = useState(true)
     const [categoryItems, setCategoryItems] = useState([])
     const slide = useRef(null)
 
     useEffect(() => {
-        const db = getFirestore()
-        const dbQuery = db.collection("items").where("category","==",category).limit(7).get()
+        const dbQuery = dbCollectionItems.where("category","==",category).limit(7).get()
         dbQuery
         .then(resp => setCategoryItems(resp.docs.map(items => ({
             id: items.id,
             ...items.data()
         }))))
+        .finally(() => setLoading(false))
 
     },[category])
 
@@ -29,18 +31,26 @@ export const Slide = ({category}) => {
     }
 
     return (
-        <li className="slide-category">
-            <div className="slide-header">
-                <h3 className="textColor"><span>{category}</span>Games</h3>
-                    <button className="btn-secondary"><Link to={`/shop/category/${category}`} className="textColor">See all</Link></button>
-            </div>
-            <ul className="slide" ref={slide}>
-                {
-                    categoryItems.map(item => <SlideItem items={item} key={item.id}/>)
-                }
-            </ul>
-            <button onClick={slideLeftClick} className="btn-tertiary slide-left"><box-icon name='chevron-left' color='#ffffff' ></box-icon></button>
-            <button onClick={slideRightClick} className="btn-tertiary slide-right"><box-icon name='chevron-right' color='#ffffff' ></box-icon></button>
-        </li>
+        <>
+        {
+            loading ?
+            <Loader />
+            :
+            <li className="slide-category">
+                <div className="slide-header">
+                    <h3 className="textColor"><span>{category}</span>Games</h3>
+                        <button className="btn-secondary"><Link to={`/shop/category/${category}`} className="textColor">See all</Link></button>
+                </div>
+                <ul className="slide" ref={slide}>
+                    {
+                        categoryItems.map(item => <SlideItem items={item} key={item.id}/>)
+                    }
+                </ul>
+                <button onClick={slideLeftClick} className="btn-tertiary slide-left"><box-icon name='chevron-left' color='#ffffff' ></box-icon></button>
+                <button onClick={slideRightClick} className="btn-tertiary slide-right"><box-icon name='chevron-right' color='#ffffff' ></box-icon></button>
+            </li>
+        }
+        </>
+        
     )
 }
