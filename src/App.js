f4271f46e01f34c8a4e69b534ea3822d5fce13ef
Alexis Ruiz Diaz <alexis00rodrigo@gmail.com>
2021-12-05 23:22:25 -0300

    Modify the routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import { NavBar } from "./components/NavBar"
+import { NavBar } from "./components/NavBar/NavBar";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { ItemListContainer } from "./components/ItemListContainer";
-import { ItemDetailsContainer } from "./components/ItemDetailsContainer";
-import { Cart } from "./components/Cart";
+import { HomeContainer } from "./components/HomeContainer/HomeContainer";
+import { ShopContainer } from "./components/ShopContainer/ShopContainer";
+import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { CartContextProvider } from "./context/CartContext";
-import "./style/style.css"
-import "boxicons"
+import { Cart } from "./components/Cart/Cart";
+import { Footer } from "./components/Footer/Footer";
+import "./style/style.css";
+import "boxicons";
+import { AddProduct } from "./components/AddProduct";
 
 function App() {
     return (
-    <>
+        <>
         <CartContextProvider>
             <BrowserRouter>
-                <header>
-                    <NavBar />
-                </header>
-                <main>
-                    <Switch>
-                        <Route exact path="/" component={ItemListContainer} />
-                        <Route exact path="/category/:idCategory" component={ItemListContainer}/>
-                        <Route exact path="/type/:idType" component={ItemListContainer}/>
-                        <Route exact path="/detail/:idDetail" component={ItemDetailsContainer} />
-                        <Route exact path="/cart" component={Cart} />
-                    </Switch>
-                </main>
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <Switch>
+                <Route exact path="/" component={HomeContainer} />
+                <Route exact path="/shop" component={ShopContainer} />
+                <Route exact path="/shop/category/:idCategory" component={ShopContainer}/>
+                <Route exact path="/shop/product/:idProduct" component={ItemDetailContainer} />
+                <Route exact path="/addProduct" component={AddProduct} />
+                <Route exact path="/cart" component={Cart} />
+                </Switch>
+            </main>
+            <footer>
+                <Footer />
+            </footer>
             </BrowserRouter>
         </CartContextProvider>
-    </>
+        </>
     );
 }
 
