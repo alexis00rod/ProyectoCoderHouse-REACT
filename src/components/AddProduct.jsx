5472a9ee820e4f16be69d659b76669da1cce40c6
Alexis Ruiz Diaz <alexis00rodrigo@gmail.com>
2021-12-05 23:27:09 -0300

    Add function to add products to the database

---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react/cjs/react.development"
+import { dbCollection } from "../services/getFirestone"
+import firebase from "firebase"
+import "firebase/firestore"
+
+
+export const AddProduct = () => {
+    const [productToAdd, setProductToAdd] = useState({})
+
+    const addProductToDB = (e) => {
+        e.preventDefault()
+        dbCollection.add(productToAdd)
+
+        console.log(`Product add!`)
+
+    }
+
+    const changeAddProduct = (e) => {
+        setProductToAdd({
+            ...productToAdd,
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+    return (
+        <section>
+            <form onChange={changeAddProduct} onSubmit={addProductToDB}>
+                <legend>Add Product</legend>
+                <div className="add-input">
+                    <label htmlFor="name">name</label>
+                    <input type="text" name="name" id="name" value={productToAdd.name} />
+                </div>
+                <div className="add-input">
+                    <label htmlFor="category">category</label>
+                    <input type="text" name="category" id="category" value={productToAdd.category} />
+                </div>
+                <div className="add-input">
+                    <label htmlFor="imgIcon">imgIcon</label>
+                    <input type="text" name="imgIcon" id="imgIcon" value={productToAdd.imgIcon} />
+                </div>
+                <div className="add-input">
+                    <label htmlFor="price">price</label>
+                    <input type="text" name="price" id="price" value={productToAdd.price} />
+                </div>
+                <div className="add-input">
+                    <label htmlFor="type">type</label>
+                    <input type="text" name="type" id="type" value={productToAdd.type} />
+                </div>
+                <div className="add-input">
+                    <label htmlFor="video">video</label>
+                    <input type="text" name="video" id="video" value={productToAdd.video} />
+                </div>
+                <div className="add-input">
+                    <label htmlFor="imgBanner">imgBanner</label>
+                    <input type="text" name="imgBanner" id="imgBanner" value={productToAdd.imgBanner} />
+                </div>
+                <div className="add-input">
+                    <label htmlFor="stock">stock</label>
+                    <input type="text" name="stock" id="stock" value={productToAdd.stock} />
+                </div>
+                <div className="add-input">
+                    <label htmlFor="description">description</label>
+                    <input type="text" name="description" id="description" value={productToAdd.description} />
+                </div>
+                <button type="submit">Add product</button>
+            </form>
+        </section>
+    )
+}
