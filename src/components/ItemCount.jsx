30e20ecabdb774ebbdf0d06be9201f319d5469c5
Alexis Ruiz Diaz <alexis00rodrigo@gmail.com>
2021-12-05 23:15:46 -0300

    Add alerts with SweetAlert

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,31 +1,50 @@
 import React,{ useState } from "react"
 import { Link } from "react-router-dom"
+import swal from "sweetalert"
 
 export const ItemCount = ({stock, initial, onAdd})=>{
     const [count, setCount] = useState(initial)
 
     const [buttonType, setButtonType] = useState("add")
 
-    const AddToCartButton = () => <button className="btn__primary" onClick={addToCart}>Add to Cart</button>
-    const ShowCartButton = () => <button className="btn__primary"><Link to="/cart" >Show Cart</Link></button>
+    const ItemCounter = () => {
+        return <div className="item-count">
+                    <div className="counter">
+                        <button onClick={removeItem}><span className="textColor">-</span></button>
+                        <span className="textColor">{count}</span>
+                        <button onClick={addItem}><span className="textColor">+</span></button>
+                    </div>
+                    <button className="btn-primary" onClick={addToCart}><span>Add to Cart</span></button>
+                </div>
+    }
 
-    const addItem = ()=> (count < stock) ? setCount(count + 1) : alert("Alcanzo el maximo de stock")
-    const removeItem = ()=> (count > initial) ? setCount(count - 1) : alert("No se puede remover mas")
+    const ShowCartButton = () => <button className="btn-primary"><Link to="/cart" >Show Cart</Link></button>
+
+    const addItem = ()=> (count < stock) ? 
+    setCount(count + 1) 
+    : 
+    swal({
+        text: "You reached the maximum stock",
+        icon: "warning"
+    })
+    const removeItem = ()=> (count > initial) ? 
+    setCount(count - 1) 
+    : 
+    swal({
+        text: "Can not be removed anymore",
+        icon: "warning"
+    })
 
     const addToCart = ()=> {
         setButtonType("show")
         onAdd(count)
     }
 
-    
     return (
-        <div className="itemCount">
-            <div className="itemCount__counter">
-                <button onClick={removeItem}>-</button>
-                <span>{count}</span>
-                <button onClick={addItem}>+</button>
-            </div>
-            { buttonType === "add" ? <AddToCartButton /> : <ShowCartButton />}
-        </div>
+        <>
+        {
+            buttonType === "add" ? <ItemCounter /> : <ShowCartButton />
+        }
+        </>
     )
 }
