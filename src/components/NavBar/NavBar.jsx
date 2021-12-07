f303f83c3fbbaba65d235d7fcf873548b358058c
Alexis Ruiz Diaz <alexis00rodrigo@gmail.com>
2021-12-07 14:55:27 -0300

    Add user icon and style when scrolling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,35 @@
-import { Link, NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { CartWidget } from "../Cart/CartWidget"
+import { useUserContext } from "../../context/UserContext"
+import { useEffect } from "react/cjs/react.development"
 
-export const NavBar = () => {
+export const NavBar = ({scroll}) => {
+    const { user } = useUserContext()
+
+    useEffect(() => {
+
+    },[])
+
     return (
-        <nav className="navbar">
-            <h1><Link to="/"><span className="textColor">Game</span>Shop</Link></h1>
-            <ul className="navbar-menu">
-                <li><NavLink to="/" activeClassName="link-active">Home</NavLink></li>
-                <li><NavLink to="/shop" activeClassName="link-active">Shop</NavLink></li>
-                <li><NavLink to="/shop/category/ps2" activeClassName="link-active">PS2</NavLink></li>
-                <li><NavLink to="/shop/category/ps3" activeClassName="link-active">PS3</NavLink></li>
-                <li><NavLink to="/shop/category/xbox360" activeClassName="link-active">XBOX 360</NavLink></li>
-                <li><CartWidget /></li>
-            </ul>
-        </nav>
+        <header className={scroll ? "scroll-active" : ""}>
+            <nav className="navbar">
+                <h1><Link to="/"><span className="textColor">Game</span>Shop</Link></h1>
+                <ul className="navbar-menu">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/shop">Shop</Link></li>
+                    <li><Link to="/shop/category/ps2">PS2</Link></li>
+                    <li><Link to="/shop/category/ps3">PS3</Link></li>
+                    <li><Link to="/shop/category/xbox360">XBOX 360</Link></li>
+                </ul>
+                <ul className="navbar-user">
+                    <li><CartWidget /></li>
+                    <li className="user-profile">
+                        <Link to="/user">
+                            <img src={user.imgProfile} alt="" />
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
